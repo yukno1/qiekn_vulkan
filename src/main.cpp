afa293b2a774aea6ba956d960b22825a6e95ef43
Yuk1no <121278340+yukno1@users.noreply.github.com>
2026-04-21 15:14:57 +0800

    base code

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,58 @@
 import vulkan;
+import std;
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
-import greeting;
+// clang-format off
+#include <cstdlib>
+// clang-format on
 
-int main() { greet("world"); }
+constexpr uint32_t WIDTH = 800;
+constexpr uint32_t HEIGHT = 600;
+
+class HelloTriangleApplication {
+public:
+  void run() {
+    initWindow();
+    initVulkan();
+    mainLoop();
+    cleanup();
+  }
+
+private:
+  GLFWwindow *window;
+
+  void initWindow() {
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+  }
+  void initVulkan() {}
+
+  void mainLoop() {
+    while (!glfwWindowShouldClose(window)) {
+      glfwPollEvents();
+    }
+  }
+
+  void cleanup() {
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+  }
+};
+
+int main() {
+  try {
+    HelloTriangleApplication app;
+    app.run();
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
