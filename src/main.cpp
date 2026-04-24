510e9776166fd4e0ae8188605a9fd9c0dc612fca
Yuk1no <121278340+yukno1@users.noreply.github.com>
2026-04-24 20:19:45 +0800

    dynamic rendering; command pool; command buffer

--- src/main.cpp
+++ src/main.cpp
@@ -62,7 +62,8 @@ private:
     // window surface
     vk::raii::SurfaceKHR surface = nullptr;
 
-    vk::raii::Queue queue = nullptr;
+    uint32_t        queueIndex = ~0;
+    vk::raii::Queue queue      = nullptr;
 
     vk::raii::SwapchainKHR           swapChain = nullptr;
     std::vector<vk::Image>           swapChainImages;
@@ -70,7 +71,11 @@ private:
     vk::Extent2D                     swapChainExtent;
     std::vector<vk::raii::ImageView> swapChainImageViews;
 
-    vk::raii::PipelineLayout pipelineLayout = nullptr;
+    vk::raii::PipelineLayout pipelineLayout   = nullptr;
+    vk::raii::Pipeline       graphicsPipeline = nullptr;
+
+    vk::raii::CommandPool   commandPool   = nullptr;
+    vk::raii::CommandBuffer commandBuffer = nullptr;
 
     std::vector<const char*> requiredDeviceExtension = {vk::KHRSwapchainExtensionName};
 
@@ -106,6 +111,8 @@ private:
         createSwapChain();
         createImageViews();
         createGraphicsPipeline();
+        createCommandPool();
+        createCommandBuffer();
     }
 
     /**
@@ -137,7 +144,7 @@ private:
 
     void createInstance()
     {
-        std::cout << "Create Instance" << std::endl;
+        std::cout << "Creating Instance" << std::endl;
         // This data is technically optional, but it may provide some useful
         // information to the driver to optimize our specific application
         constexpr vk::ApplicationInfo appInfo{.pApplicationName   = "Hello Triangle",
@@ -204,7 +211,7 @@ private:
 
     void setupDebugMessenger()
     {
-        std::cout << "Set Up DebugMessenger" << std::endl;
+        std::cout << "Setting Up DebugMessenger" << std::endl;
         if (!enableValidationLayers) return;
 
         vk::DebugUtilsMessageSeverityFlagsEXT severityFlags(
@@ -299,7 +306,6 @@ private:
 
         // get the first index into queueFamilyProperties which supports both
         // graphics and present
-        uint32_t queueIndex = ~0;
         for (uint32_t qfpIndex = 0; qfpIndex < queueFamilyProperties.size(); qfpIndex++)
         {
             if ((queueFamilyProperties[qfpIndex].queueFlags & vk::QueueFlagBits::eGraphics) &&
@@ -460,6 +466,129 @@ private:
                                                         .pushConstantRangeCount = 0};
 
         pipelineLayout = vk::raii::PipelineLayout(device, pipelineLayoutInfo);
+
+        vk::StructureChain<vk::GraphicsPipelineCreateInfo, vk::PipelineRenderingCreateInfo>
+            pipelineCreateInfoChain = {{.stageCount          = 2,
+                                        .pStages             = shaderStages,
+                                        .pVertexInputState   = &vertexInputInfo,
+                                        .pInputAssemblyState = &inputAssembly,
+                                        .pViewportState      = &viewportState,
+                                        .pRasterizationState = &rasterizer,
+                                        .pMultisampleState   = &multisampling,
+                                        .pColorBlendState    = &colorBlending,
+                                        .pDynamicState       = &dynamicState,
+                                        .layout              = pipelineLayout,
+                                        .renderPass          = nullptr},
+                                       {.colorAttachmentCount    = 1,
+                                        .pColorAttachmentFormats = &swapChainSurfaceFormat.format}};
+
+        graphicsPipeline = vk::raii::Pipeline(
+            device, nullptr, pipelineCreateInfoChain.get<vk::GraphicsPipelineCreateInfo>());
+    }
+
+    void createCommandPool()
+    {
+        std::cout << "Creating Command Pool" << std::endl;
+        vk::CommandPoolCreateInfo poolInfo{.flags =
+                                               vk::CommandPoolCreateFlagBits::eResetCommandBuffer,
+                                           .queueFamilyIndex = queueIndex};
+        commandPool = vk::raii::CommandPool(device, poolInfo);
+    }
+
+    void createCommandBuffer()
+    {
+        std::cout << "Creating Command Buffer" << std::endl;
+        vk::CommandBufferAllocateInfo allocInfo{.commandPool = commandPool,
+                                                .level       = vk::CommandBufferLevel::ePrimary,
+                                                .commandBufferCount = 1};
+
+        commandBuffer = std::move(vk::raii::CommandBuffers(device, allocInfo).front());
+    }
+
+    void recordCommandBuffer(uint32_t imageIndex)
+    {
+        std::cout << "Recording Command Buffer" << std::endl;
+        commandBuffer.begin({});
+
+        // Before starting rendering, transition the swapchain image to
+        // vk::ImageLayout::eColorAttachmentOptimal
+        transition_image_layout(imageIndex,
+                                vk::ImageLayout::eUndefined,
+                                vk::ImageLayout::eColorAttachmentOptimal,
+                                {},   // srcAccessMask (no need to wait for previous operations)
+                                vk::AccessFlagBits2::eColorAttachmentWrite,   // dstAccessMask
+                                vk::PipelineStageFlagBits2::eColorAttachmentOutput,   // srcStage
+                                vk::PipelineStageFlagBits2::eColorAttachmentOutput    // dstStage
+        );
+
+        // Set up the color attachment
+        vk::ClearValue              clearColor     = vk::ClearColorValue(0.0f, 0.0f, 0.0f, 1.0f);
+        vk::RenderingAttachmentInfo attachmentInfo = {.imageView = swapChainImageViews[imageIndex],
+                                                      .imageLayout =
+                                                          vk::ImageLayout::eColorAttachmentOptimal,
+                                                      .loadOp     = vk::AttachmentLoadOp::eClear,
+                                                      .storeOp    = vk::AttachmentStoreOp::eStore,
+                                                      .clearValue = clearColor};
+
+        // set up the rendering info
+        vk::RenderingInfo renderingInfo = {
+            .renderArea           = {.offset = {0, 0}, .extent = swapChainExtent},
+            .layerCount           = 1,
+            .colorAttachmentCount = 1,
+            .pColorAttachments    = &attachmentInfo};
+
+        commandBuffer.beginRendering(renderingInfo);
+
+        // bind the graphics pipeline
+        commandBuffer.bindPipeline(vk::PipelineBindPoint::eGraphics, *graphicsPipeline);
+
+        commandBuffer.setViewport(0,
+                                  vk::Viewport(0.0f,
+                                               0.0f,
+                                               static_cast<float>(swapChainExtent.width),
+                                               static_cast<float>(swapChainExtent.height),
+                                               0.0f,
+                                               1.0f));
+        commandBuffer.setScissor(0, vk::Rect2D(vk::Offset2D(0, 0), swapChainExtent));
+        commandBuffer.draw(3, 1, 0, 0);
+        commandBuffer.endRendering();
+
+        // After rendering, transition the swapchain image to vk::ImageLayout::ePresentSrcKHR
+        transition_image_layout(imageIndex,
+                                vk::ImageLayout::eColorAttachmentOptimal,
+                                vk::ImageLayout::ePresentSrcKHR,
+                                vk::AccessFlagBits2::eColorAttachmentWrite,   // srcAccessMask
+                                {},                                           // dstAccessMask
+                                vk::PipelineStageFlagBits2::eColorAttachmentOutput,   // srcStage
+                                vk::PipelineStageFlagBits2::eBottomOfPipe             // dstStage
+        );
+        commandBuffer.end();
+    }
+
+    void transition_image_layout(uint32_t imageIndex, vk::ImageLayout old_layout,
+                                 vk::ImageLayout new_layout, vk::AccessFlags2 src_access_mask,
+                                 vk::AccessFlags2        dst_access_mask,
+                                 vk::PipelineStageFlags2 src_stage_mask,
+                                 vk::PipelineStageFlags2 dst_stage_mask)
+    {
+        vk::ImageMemoryBarrier2 barrier = {
+            .srcStageMask        = src_stage_mask,
+            .srcAccessMask       = src_access_mask,
+            .dstStageMask        = dst_stage_mask,
+            .dstAccessMask       = dst_access_mask,
+            .oldLayout           = old_layout,
+            .newLayout           = new_layout,
+            .srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED,
+            .dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED,
+            .image               = swapChainImages[imageIndex],
+            .subresourceRange    = {.aspectMask     = vk::ImageAspectFlagBits::eColor,
+                                    .baseMipLevel   = 0,
+                                    .levelCount     = 1,
+                                    .baseArrayLayer = 0,
+                                    .layerCount     = 1}};
+        vk::DependencyInfo dependency_info = {
+            .dependencyFlags = {}, .imageMemoryBarrierCount = 1, .pImageMemoryBarriers = &barrier};
+        commandBuffer.pipelineBarrier2(dependency_info);
     }
 
     [[nodiscard]] vk::raii::ShaderModule createShaderModule(const std::vector<char>& code) const
